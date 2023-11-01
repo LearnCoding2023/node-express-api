9733e872ef2f71dd97ba0bbcc621020c4ba6a6c2
LearnCoding2023 <tyson.fi89@gmail.com>
2023-11-01 21:33:25 +0530

    update docker changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3001, () => console.log('Application listening on 3001'))
+app.listen(3000, () => console.log('Application listening on 3001'))
 
 app.get('/', (req, res) => res.json({message: 'SUCCESS'}))
